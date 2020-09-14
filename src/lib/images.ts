71e5900e7466bf69091a9f4fbf8fc94a8b345359
Joep van de Laarschot <joep@jodibooks.com>
2020-09-14 11:44:13 +0200

    Update banners, logo and favicons

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,15 +1,11 @@
 export const images = { 
+    'assets/banner-blog-default.png': require('../assets/banner-blog-default.png'),
     'assets/banner-blog.png': require('../assets/banner-blog.png'),
-    'assets/banner-com.png': require('../assets/banner-com.png'),
-    'assets/banner-howto.png': require('../assets/banner-howto.png'),
-    'assets/banner-websites.png': require('../assets/banner-websites.png'),
-    'assets/banner.png': require('../assets/banner.png'),
     'assets/blog/authors/jj.jpeg': require('../assets/blog/authors/jj.jpeg'),
     'assets/blog/authors/joe.jpeg': require('../assets/blog/authors/joe.jpeg'),
     'assets/blog/authors/tim.jpeg': require('../assets/blog/authors/tim.jpeg'),
     'assets/blog/dynamic-routing/cover.jpg': require('../assets/blog/dynamic-routing/cover.jpg'),
     'assets/blog/hello-world/cover.jpg': require('../assets/blog/hello-world/cover.jpg'),
     'assets/blog/preview/cover.jpg': require('../assets/blog/preview/cover.jpg'),
-    'assets/home-banner-beach.jpg': require('../assets/home-banner-beach.jpg'),
-    'assets/home-banner.jpg': require('../assets/home-banner.jpg'),
+    'assets/logo-blog.png': require('../assets/logo-blog.png'),
 }
