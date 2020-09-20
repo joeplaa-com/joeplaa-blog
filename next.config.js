2d79d3c4fa867cb9fc7b1c2b8fda63e09cef6f7b
Joep van de Laarschot <joep@jodibooks.com>
2020-09-20 14:29:11 +0200

    uncrease node listeners to 20

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
+process.setMaxListeners(20);
+
 module.exports = withBundleAnalyzer(
     withOptimizedImages({
         webpack(config) {
