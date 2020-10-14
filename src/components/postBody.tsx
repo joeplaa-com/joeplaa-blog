29933137c154e4423a8d7e71dc3325c72dc4a724
Joep van de Laarschot <joep@jodibooks.com>
2020-10-14 15:46:00 +0200

    cleanup components

--- src/components/postBody.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { PostBodyProps } from '../types'
-
-export default function PostBody({ content }: PostBodyProps) {
-    return (
-        <MDXRenderer>{content}</MDXRenderer>
-    );
-}
