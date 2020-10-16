0254866863aa9f6ead6463b412e96e3c7b4e5e06
Joep van de Laarschot <joep@jodibooks.com>
2020-10-16 19:26:45 +0200

    Updated lambda functions

--- server/lambda/LambdaWebserverAndRedirects/handler.js
+++ server/lambda/LambdaWebserverAndRedirects/handler.js
@@ -4,8 +4,58 @@
  * Or see the copy below
  */
 
-exports.handler = function handler (event, context, callback) {
+const redirects = [
+    '/the-news',
+    '/our-emotional-seesaw',
+    '/whiteness-defined',
+    '/low-salt-intake-is-bad',
+    '/newspeak',
+    '/rule-2-treat-yourself-like-someone-you-are-responsible-for-helping',
+    '/not-a-political-journey-too',
+    '/living-under-autism',
+    '/im-on-edge',
+    '/document',
+    '/what-i-learned-from-migrating-our-websites',
+    '/tearing-down-the-house',
+    '/lessons-from-my-running-failure',
+    '/learning-to-code',
+    '/do-i-have-a-sugar-intolerance',
+    '/customer-service',
+    '/keep-asking-why-a-letter-to-my-niece-and-nephew',
+    '/intermittent-fasting',
+    '/my-priorities',
+    '/my-favorite-quotes',
+    '/whats-the-right-diet-for-me',
+    '/the-struggle-for-perfection',
+    '/the-ultimate-why-improve-things',
+    '/what-i-learned-this-year',
+    '/my-goals-a-little-more-context-part-2',
+    '/my-goals-a-little-more-context-part-1',
+    '/joeps-goals',
+    '/running-for-me',
+    '/why-do-most-parties-start-around-midnight',
+    '/please-teach-me',
+    '/i-love-the-beach'
+]
+
+exports.handler = (event, context, callback) => {
     const { request } = event.Records[0].cf;
+
+    // Check for blog redirects first
+    if (redirects.includes(request.headers.host[0].value)) {
+        const response = {
+            status: '301',
+            statusDescription: `Redirecting to blog domain`,
+            headers: {
+                location: [{
+                    key: 'Location',
+                    value: `https://blog.joeplaa.com${request.uri}`
+                }]
+            }
+        };
+        callback(null, response);
+        return;
+    }
 
     // If no "." in URI, assume document request and append index.html to request.uri
     if (request.uri.match(/^[^.]*$/)) {
@@ -17,5 +67,5 @@ exports.handler = function handler (event, context, callback) {
     }
 
     // Return to CloudFront Origin Request
-    return callback(null, request);
+    callback(null, request);
 };
