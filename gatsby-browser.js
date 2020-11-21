52f259076b4a3a18780e70860f9756c2fa4408aa
Joep van de Laarschot <joep@jodibooks.com>
2020-11-21 13:59:01 +0100

    fixes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 // https://stackoverflow.com/questions/61967988/how-to-create-page-with-parameters-in-gatsbyjs-dynamically
+import React from 'react'
 import { wrapRootElement as wrap } from './root-wrapper'
 import Layout from './src/components/layout'
 
--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -19,7 +19,7 @@ export default function Filter({ buttonType, className, page, quantity, tags }: 
                 ? <Col xs='auto' className='mt-2 mt-sm-0 ml-auto'>
                     <Button outline color='primary' onClick={buttonType === 'more' ? () => location.href=page : () => navigate(-1)} className="d-inline-flex justify-content-center align-items-center" aria-label='filter-button'>
                         <IconContext.Provider value={{ size: '1.25rem' }}><MdArrowBack /></IconContext.Provider>
-                        <span className={'d-none d-sm-flex mr-1'}>{buttonType === 'more' ? content.MorePosts : content.Back}</span>
+                        <span className={'d-none d-sm-flex mr-1'}>{buttonType === 'more' ? content.More : content.Back}</span>
                     </Button>
                 </Col>
                 : null}
