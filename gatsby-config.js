df3349c979544485e6a47fdbf615e7bbea8f3ae7
Joep van de Laarschot <joep@jodibooks.com>
2021-10-08 17:23:23 +0200

    fix umamiID

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ const siteMetadata = {
         designedBy: 'Website design by',
         designerName: 'Joeplaa',
         designerUrl: 'https://github.com/joeplaa/joeplaa.com',
-        plausibleID: process.env.GATSBY_PLAUSIBLE_ID
+        umamiID: 'a8a7d2b3-3217-4c64-9c95-a25e7b18e5f9'
     },
     siteUrl: process.env.GATSBY_BLOG_URL, // needed for gatsby-plugin-advanced-sitemap
     urls: {
